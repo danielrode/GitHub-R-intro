277670f92076f8ef2922e07c5242d488bc87acf1
Daniel Rode <daniel.rode@colostate.edu>
2023-01-20 15:14:20 -0700

    Class week 1, day 1: Exercise sets 6.3.1 and 6.3.2

---
+++ setup.R
@@ -0,0 +1,27 @@
+
+packageLoad <-
+  function(x) {
+    for (i in 1:length(x)) {
+      if (!x[i] %in% installed.packages()) {
+        install.packages(x[i])
+      }
+      library(x[i], character.only = TRUE)
+    }
+  }
+
+
+# create a string of package names
+packages <- c('tidyverse',
+              'palmerpenguins',
+              'sf',
+              'terra',
+              'tmap',
+              'rmarkdown',
+              'tigris',
+              'elevatr',
+              'rgdal')
+
+packageLoad(packages)
+
+
+# call this script in future assignments using `source("PATH_TO_THIS_SCRIPT")`
